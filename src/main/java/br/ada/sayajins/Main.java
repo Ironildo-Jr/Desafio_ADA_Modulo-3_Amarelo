3668e9f51c7c75c6f4a7de458171303d20a9f6be
F7023302 Matheus Rodrigues de Jesus <matheus.jesus@bb.com.br>
2022-12-05 16:16:29 +0000

    Add credito 3 por cento

--- src/main/java/br/ada/sayajins/Main.java
+++ src/main/java/br/ada/sayajins/Main.java
@@ -36,6 +36,8 @@ public class Main {
             }
             pagamentos.stream().forEach(System.out::println);
         }
+
+        
     }
 
 }
--- src/main/java/br/ada/sayajins/model/Pagamentos.java
+++ src/main/java/br/ada/sayajins/model/Pagamentos.java
@@ -3,7 +3,10 @@ package br.ada.sayajins.model;
 import java.math.BigDecimal;
 import java.text.NumberFormat;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.YearMonth;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.Locale;
 
 public class Pagamentos {
@@ -47,31 +50,57 @@ public class Pagamentos {
         this.nome = v;
     }
 
-
-    public Pagamentos(String s){
+    public Pagamentos(String s) {
         setNome(s);
     }
 
-    public Pagamentos(String s, LocalDate dateVencto, double valor, TipoPagamentoEnum tipoPagamentoEnum){
-        this.valor=BigDecimal.valueOf(valor);
-        this.nome=s;
-        this.dtVencto=dateVencto;
-        this.tipoPagamentoEnum=tipoPagamentoEnum;
+    public BigDecimal getValorTotal() {
+
+        // Duration duracao = Duration.between(this.dtVencto,LocalDateTime.now());
+
+        if (this.dtVencto.isBefore(LocalDate.now())) {
+
+            Long monthsBetween = ChronoUnit.MONTHS.between(
+                    YearMonth.from(this.dtVencto),
+                    YearMonth.from(LocalDateTime.now()));
+
+            System.out.println(monthsBetween); // 3
+
+            if (tipoPagamentoEnum.equals(TipoPagamentoEnum.CREDITO)) {
+                return this.valor.multiply((new BigDecimal("1"))
+                        .add(new BigDecimal("0.03").multiply(new BigDecimal(monthsBetween.toString()))));
+            }
+        }
+        return this.valor;
     }
 
-
-    public String getValorFormatado(){
-        NumberFormat formatter = NumberFormat.getCurrencyInstance(new Locale("pt","BR"));
-        return  formatter.format(this.valor.doubleValue());
+    public Pagamentos(String s, LocalDate dateVencto, double valor, TipoPagamentoEnum tipoPagamentoEnum) {
+        this.valor = BigDecimal.valueOf(valor);
+        this.nome = s;
+        this.dtVencto = dateVencto;
+        this.tipoPagamentoEnum = tipoPagamentoEnum;
     }
+
+    public String getValorFormatado() {
+        NumberFormat formatter = NumberFormat.getCurrencyInstance(new Locale("pt", "BR"));
+        return formatter.format(this.valor.doubleValue());
+    }
+
+    public String getValorTotalFormatado() {
+        NumberFormat formatter = NumberFormat.getCurrencyInstance(new Locale("pt", "BR"));
+        return formatter.format(this.getValorTotal().doubleValue());
+    }
+
     @Override
-    public String toString(){
+    public String toString() {
         return this.nome
-                   .concat("   |   ")
-                   .concat(this.dtVencto.format(DateTimeFormatter.ofPattern("dd/MM/YYYY")))
-                   .concat("   |   ")
-                   .concat(this.getValorFormatado())
-                   .concat("   |   ")
-                   .concat(this.getTipoPagamentoEnum().name());
+                .concat("   |   ")
+                .concat(this.dtVencto.format(DateTimeFormatter.ofPattern("dd/MM/YYYY")))
+                .concat("   |   ")
+                .concat(this.getValorFormatado())
+                .concat("   |   ")
+                .concat(this.getValorTotalFormatado())
+                .concat("   |   ")
+                .concat(this.getTipoPagamentoEnum().name());
     }
 }
